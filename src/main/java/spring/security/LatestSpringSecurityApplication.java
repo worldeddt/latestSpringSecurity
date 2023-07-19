30513250ea9aec4d8b56116eb7e8da9c6bfe7cd8
김태형 <ktest92@gmail.com>
2023-07-19 18:11:01 +0900

    first commit

---
+++ src/main/java/spring/security/LatestSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatestSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LatestSpringSecurityApplication.class, args);
+    }
+
+}
