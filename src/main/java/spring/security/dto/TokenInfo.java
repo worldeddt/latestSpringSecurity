e00192a52599b4470b486a26965e2674899ef2da
김태형 <ktest92@gmail.com>
2023-07-20 08:48:40 +0900

    provider algorithm

---
+++ src/main/java/spring/security/dto/TokenInfo.java
@@ -0,0 +1,15 @@
+package spring.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+@AllArgsConstructor
+public class TokenInfo {
+
+    private String grantType;
+    private String accessToken;
+    private String refreshToken;
+}
